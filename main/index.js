74a7f566d293d5f39145f4d958a2ce2e85496503
akash <infoaj549814@gmail.com>
2021-01-27 14:30:26 +0600

    add

--- main/index.js
+++ main/index.js
@@ -8,36 +8,8 @@ loginBtn.addEventListener("click", function () {
 })
 
 // deposit button event handler
-const depositBtn = document.getElementById("currentDeposit")
+const depositBtn = document.getElementById("addDeposit")
 depositBtn.addEventListener("click", function () {
-    const depositNumber = getInputNumber("depositAmount")
+console.log("clicked");
 
-    updateSpanText("currentDeposit", depositNumber)
-    updateSpanText("currentBalance", depositBalance)
-    document.getElementById("depositAmount").value = ""
-
-})
-
-
-
-// withdraw button event handler
-const withDrawBtn = document.getElementById("addWithdraw")
-withDrawBtn.addEventListener("click", function () {
-    const withdrawNumber = getInputNumber("withdrawAmount")
-    updateSpanText("currentWithdraw", withdrawNumber)
-    updateSpanText("currentBalance", -1 * withdrawNumber)
-    document.getElementById("withdrawAmount").value = ""
-})
-
-function getInputNumber(id) {
-    const amount = document.getElementById(id).value
-    const amountNumber = parseFloat(amount)
-    return amountNumber
-}
-function updateSpanTExt(id,addedNumber) {
-    const current =document.getElementById(id).innerText
-    const currentNumber = parseFloat (current)
-    const totalAmount =addedNumber + currentNumber
-    document.getElementById(id).innerText = totalAmount
-    
 }
